288a8867ec7decc42bf06c51ab49b507f4ea285c
jmss127 <jmss127@protonmail.com>
2025-07-18 17:54:44 +0100

    initial commit for shark an enigma like encoding machine.

---
+++ shark.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# File: shark.sh
+# Author: jms
+# Date: 18 Jul 2025
+# ------------------------------------------------------------------------------
+# Comments:
+# ------------------------------------------------------------------------------
+# Setup & Config
+
+# Resolve script's directory
+# SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Color Variables tput
+# r=$(tput setaf 1) # Warnings/errors Red
+# g=$(tput setaf 2) # Menus/info/success Green
+# c=$(tput setaf 6) # Input prompts Cyan
+# m=$(tput setaf 5) # Highlights Magenta
+# reset=$(tput sgr0) # Reset
+#
+# trap 'echo -e "$reset"; exit 0' INT TERM EXIT
+# ------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------
+# Main
